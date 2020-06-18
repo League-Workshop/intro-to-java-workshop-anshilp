2f197ef98b236bd2aac5bc7395313a059f04d6c1
root <root@1dee67862b79>
2020-06-18 21:56:11 +0000

    Day 4

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -11,6 +11,24 @@ public class DragonFight {
 		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
+		
+		int playerHealth = 100;
+		int dragonHealth = 100;
+		int playerDamage = 0;
+		int dragonDamage = 0;
+		
+	while(playerHealth>0 && dragonHealth>0) {
+		String answer = JOptionPane.showInputDialog("Do you want to yell at the dragon, or kick the dragon");
+		if (answer.equalsIgnoreCase("yell")) {
+			int random = r.nextInt(11);
+			random=-dragonHealth; 
+			
+		}
+
+		
+	}	
+		
+		
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
 	
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
@@ -61,5 +79,58 @@ public class DragonFight {
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
 			
 		}
+
+	// 1. Make a main method that includes all the steps below…
+	public static void main(String[] args) {
+		
+		// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+		
+		 Random r = new Random();
+		 int random =  r.nextInt(4); // Anything between 0 to 3, excluding 4.
+	
+		
+		 JOptionPane.showMessageDialog(null, random);
+		 JOptionPane.showInputDialog("What is something you think is awesome?");
+		 
+		 if(random==0) {
+			 JOptionPane.showMessageDialog(null, "That's Awesome!");
+		 }
+		 
+		 if(random==1)	{ 
+			 JOptionPane.showMessageDialog(null, "That's okay.");
+		 }
+		 
+		 if(random==2)	{
+			 JOptionPane.showMessageDialog(null, "That's boring!");
+		 }
+		 
+		 if(random==3)	{
+			 JOptionPane.showMessageDialog(null, "Cool!");
+		 }
+		 
+		 
+		 
+		// 3. Print out this variable
+	
+		// 4. Get the user to enter something that they think is awesome
+	
+		// 5. If the random number is 0
+		
+	
+	
+		// -- tell the user whatever they entered is awesome!
+	
+		// 6. If the random number is 1
+	
+		// -- tell the user whatever they entered is ok.
+	
+		// 7. If the random number is 2
+	
+		// -- tell the user whatever they entered is boring.
+	
+		// 8. If the random number is 3
+	
+		// -- write your own answer
+	}
 	}
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,9 +1,46 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
+		int score = 0;
+		
+
+		
+	String one = JOptionPane.showInputDialog("What is the sun?");
+		if (one.equalsIgnoreCase("a Star")){
+			score=score+1;
+			}
+		JOptionPane.showMessageDialog(null, score);
+		
+	String two = JOptionPane.showInputDialog("What is the year?");
+	if (two.equals("2020")) {
+		score = score+1;
+	}
+	
+	JOptionPane.showMessageDialog(null, score);
+		
+	String three = JOptionPane.showInputDialog("What year did the US join World War 2");
+	if (three.equals("1918")) {
+		score = score +1;
+	}
+	
+	JOptionPane.showMessageDialog(null, score);
+	
+	String four = JOptionPane.showInputDialog("What is your name?");
+	
+	score = score+1; 
+
+	JOptionPane.showMessageDialog(null, score);	
+
+}
+
+	
+	
+	
 		// 1.  Create a variable to hold the user's score 
 		
 		// 2.  Ask the user a question 
@@ -17,4 +54,4 @@ public class QuizGame {
 		// 6.  After all the questions have been asked, print the user's score 
 		
 	}
-}
+
